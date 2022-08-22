bcf522b2f8a3e476a646965d1bebf0bfe42d7421
Clayton Rafael <clayton.rssouza@gmail.com>
2022-08-22 16:41:41 -0300

    feat: create participant component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Participant } from '../../components/Participant'
 import { styles } from './styles'
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log('você clicou na ação de adicionar o participante')
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`você clicou na ação de remover o participante: ${name}`)
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do evento"
+          placeholderTextColor="#6b6b6b"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Participant onRemove={handleParticipantRemove} name="Clayton" />
+        <Participant onRemove={handleParticipantRemove} name="Diego" />
+        <Participant onRemove={handleParticipantRemove} name="Rodrigo" />
+        <Participant onRemove={handleParticipantRemove} name="Biro" />
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,32 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+
+    marginTop: 16,
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    padding: 16,
+    color: '#fff',
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 12,
+    marginBottom: 42,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
 })
